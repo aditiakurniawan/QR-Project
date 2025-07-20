a8d073b69e10d55d0eb5f73afe712a8c45769831
aditiakurniawanx <aditiakurniawanx@gmail.com>
2025-07-20 16:46:18 +0000

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-// Add JS here
+const dataInput = document.getElementById('data');
+const generateButton = document.getElementById('generate');
+const qrcodeContainer = document.getElementById('qrcode');
+
+generateButton.addEventListener('click', () => {
+    const data = dataInput.value;
+    if (data) {
+        // Gunakan qrcode.js
+        new QRCode(qrcodeContainer, {
+            text: data,
+            width: 256,
+            height: 256
+        });
+
+        // Atau, jika menggunakan API, kirim data ke API dan tampilkan hasilnya
+    } else {
+        alert('Silakan masukkan data!');
+    }
+});
